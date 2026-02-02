338abc6dba0efab8f0efc6b5619331cdcbe27dce
최원서 <cws98tiger@naver.com>
2026-02-03 02:51:07 +0900

    Fix: vite-plugin-pages 설정에 dirs, extensions 명시

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import Pages from "vite-plugin-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), Pages()],
+  plugins: [
+    react(),
+    Pages({
+      dirs: "src/pages",
+      extensions: ["tsx"],
+    }),
+  ],
 });
